22e8f8f6168de847421ca3af5b01aa41c5cbd709
Carly Gerber <carlyagerber@gmail.com>
2020-12-23 16:20:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,7 @@
-def using_push(array,string)
+def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift(array,string)
+def using_unshift(array, string)
+  string.unshift(array)
+end
